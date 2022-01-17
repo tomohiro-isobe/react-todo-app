9d92b87b58de4cbced15bdae585004ac619d693f
tomohiro-isobe <36503812+tomohiro-isobe@users.noreply.github.com>
2022-01-17 14:26:26 +0900

    削除機能

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...imcompleteTodos];
+    newTodos.splice(index, 1);
+    setImcompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -31,12 +37,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {imcompleteTodos.map((todo) => {
+          {imcompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
