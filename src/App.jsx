53121dcd47bc8e9730f6e22c96cac78e729a9eea
tomohiro-isobe <36503812+tomohiro-isobe@users.noreply.github.com>
2022-01-17 16:31:26 +0900

    todo上限設定

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTODO５個まで！消化してください。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
